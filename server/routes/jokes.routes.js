7b87840bb675d02eea05f1d65fa8d9f4fc0474ac
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-26 14:35:04 -0500

    Fix JokeSchema valdiation

--- server/routes/jokes.routes.js
+++ server/routes/jokes.routes.js
@@ -4,12 +4,10 @@ const JokeSchema = new mongoose.Schema(
     setup: {
       type: String,
       minlength: [10, "Setup must be at least 10 characters long"],
-      required: [true, "Setup is required"],
     },
     punchline: {
       type: String,
       minlength: [3, "Punchline must be at least 3 characters long"],
-      required: [true, "Punchline is required"],
     },
   },
   { timestamps: true }
