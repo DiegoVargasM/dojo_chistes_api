c433b5f0ba5f861a9c838e03823f3a0198f6a4d9
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-26 14:33:37 -0500

    Add JokesSchema validation

--- server/routes/jokes.routes.js
+++ server/routes/jokes.routes.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
-const JokeSchema = new mongoose.Schema({
-  setup: String,
-  punchline: String,
-});
-const Joke = mongoose.model("Joke", JokeSchema);
-module.exports = Joke;
+const JokeSchema = new mongoose.Schema(
+  {
+    setup: {
+      type: String,
+      minlength: [10, "Setup must be at least 10 characters long"],
+      required: [true, "Setup is required"],
+    },
+    punchline: {
+      type: String,
+      minlength: [3, "Punchline must be at least 3 characters long"],
+      required: [true, "Punchline is required"],
+    },
+  },
+  { timestamps: true }
+);
